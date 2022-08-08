e5b64fb833331a967959a447fd175077bfd6634e
deehombs <deehombs@gmail.com>
2022-08-08 16:41:05 +0100

    completed-ish

--- todo.js
+++ todo.js
@@ -1,37 +1,39 @@
+
+
 //To Do List JS
 document.querySelector('#push').onclick = function(){
-  if(document.querySelector('#newtask input').value.length == 0){
-      alert("Kindly Enter Task Name!!!!")
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Kindly Enter Task Name!!!!")
+    }
+  
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+  
+        const current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
   }
-
-  else{
-      document.querySelector('#tasks').innerHTML += `
-          <div class="task">
-              <span id="taskname">
-                  ${document.querySelector('#newtask input').value}
-              </span>
-              <button class="delete">
-                  <i class="far fa-trash-alt"></i>
-              </button>
-          </div>
-      `;
-
-      const current_tasks = document.querySelectorAll(".delete");
-      for(var i=0; i<current_tasks.length; i++){
-          current_tasks[i].onclick = function(){
-              this.parentNode.remove();
-          }
-      }
-  }
-}
-
-date = new Date();
-year = date.getFullYear();
-month = date.getMonth() + 1;
-day = date.getDate();
-document.getElementById("currentDate").textContent = month + "/" + day + "/" + year;
-
-const funcLocal = () => {
-  localStorage.setItem('Local Key', 'Input Value');
-};
- funcLocal ();
+  
+  date = new Date();
+  year = date.getFullYear();
+  month = date.getMonth() + 1;
+  day = date.getDate();
+  document.getElementById("currentDate").textContent = month + "/" + day + "/" + year;
+  
+  const funcLocal = () => {
+    localStorage.setItem('Local Key', 'Input Value');
+  };
+   funcLocal ();
